380f8a973facd6c18a2e7d9ca34993b41c8cd390
Rod Laycock <rod.laycock@gmail.com>
2023-03-18 12:22:57 +0000

    Added vector to store vivarium and sensors

--- include/Vivaria.h
+++ include/Vivaria.h
@@ -1,12 +1,15 @@
 /*
   Header file for the Vivaria - one or more Vivarium.
 */
+#include "Vivarium.h"
 
 #include <string>
+#include <vector>
 
 class Vivaria {
    
   private:
+    std::vector<Vivarium> vivariums;
 
   public:
     std::string title;
--- include/Vivarium.h
+++ include/Vivarium.h
@@ -1,12 +1,15 @@
 /*
   Header file for the Vivarium.
 */
+#include "Sensor.h"
 
 #include <string>
+#include <vector>
 
 class Vivarium {
    
   private:
+    std::vector<Sensor> sensors;
 
   public:
     std::string name;
