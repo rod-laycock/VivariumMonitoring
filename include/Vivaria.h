d02e5d06f57f0d1fd81f1f32c96b9cb1e228c5b2
Rod Laycock <rod.laycock@gmail.com>
2023-03-18 12:13:40 +0000

    Added Vivaria.cpp/h

---
+++ include/Vivaria.h
@@ -0,0 +1,15 @@
+/*
+  Header file for the Vivaria - one or more Vivarium.
+*/
+
+#include <string>
+
+class Vivaria {
+   
+  private:
+
+  public:
+    std::string title;
+    
+    Vivaria(std::string title);
+};
---
+++ src/Vivaria.cpp
@@ -0,0 +1,6 @@
+#include "Vivaria.h"
+
+Vivaria::Vivaria(std::string title) :
+  title(title)
+{
+};
