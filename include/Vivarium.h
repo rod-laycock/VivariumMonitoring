23d3d59b2d79094181fd5472d98b3cde242bfa32
Rod Laycock <rod.laycock@gmail.com>
2023-03-18 12:10:32 +0000

    Added Vivarium.h/cpp

---
+++ include/Vivarium.h
@@ -0,0 +1,16 @@
+/*
+  Header file for the Vivarium.
+*/
+
+#include <string>
+
+class Vivarium {
+   
+  private:
+
+  public:
+    std::string name;
+    std::string location;
+
+    Vivarium(std::string name, std::string location);
+};
---
+++ src/Vivarium.cpp
@@ -0,0 +1,6 @@
+#include "Vivarium.h"
+
+Vivarium::Vivarium(std::string name, std::string location) :
+  name(name), location(location)
+{
+};
